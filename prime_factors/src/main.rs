76d159c66ab488c5bda2431f3f3d1e0043762a3e
Ivan Panico <iv.panico@gmail.com>
2019-02-15 23:58:56 +0100

    final version of prime factors

--- prime_factors/src/main.rs
+++ prime_factors/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    const NUMBER: u64 = 600_851_475_143;
+    let mut number: u64 = NUMBER;
+    let mut result: Vec<u64> = Vec::new();
+
+    if number % 2 == 0 {
+        number = drain_prime(2, number);
+        result.push(1);
+    }
+
+    let mut i = 3;
+    while (i * i) < NUMBER {
+        if number % i == 0 {
+            number = drain_prime(i, number);
+            result.push(i);
+        }
+        i += 2;
+    }
+
+    println!("les diviseurs premiers de {} sont {:?}", NUMBER, result);
+}
+
+fn drain_prime (prime: u64, mut number: u64) -> u64 {
+    while number % prime == 0 {
+        number /= prime;
+    }
+    number
 }
